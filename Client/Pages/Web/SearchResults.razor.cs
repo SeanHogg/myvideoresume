301536734ba02cecd6d09c13882c8f4637afde22
Sean Hogg <seanhogg@gmail.com>
2024-11-19 22:55:04 -0500

    MYV-26 Changed Render to Server

---
+++ Client/Pages/Web/SearchResults.razor.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.JSInterop;
+using Newtonsoft.Json.Linq;
+using Radzen;
+using Radzen.Blazor;
+using System.ComponentModel;
+using System.Net.Http.Json;
+
+namespace MyVideoResume.Client.Pages.Web;
+
+public partial class SearchResults
+{
+    [Inject]
+    protected IJSRuntime JSRuntime { get; set; }
+
+    [Inject]
+    protected NavigationManager NavigationManager { get; set; }
+
+    [Inject]
+    protected DialogService DialogService { get; set; }
+
+    [Inject]
+    protected TooltipService TooltipService { get; set; }
+
+    [Inject]
+    protected ContextMenuService ContextMenuService { get; set; }
+
+    [Inject]
+    protected NotificationService NotificationService { get; set; }
+
+    [Inject]
+    protected SecurityService Security { get; set; }
+
+
+    [SupplyParameterFromQuery(Name = "Query")]
+    private string? Query { get; set; }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await JSRuntime.InvokeVoidAsync("BlazorSetTitle", Query);
+    }
+
+}
