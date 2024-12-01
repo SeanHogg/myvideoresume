9ab9630d0ff7e0c3d15f1f64fa382dac0b5b1570
Sean Hogg <seanhogg@gmail.com>
2024-12-01 14:08:37 -0500

    MYV-33 Job Posting

--- MyVideoResume.Abstractions/Job/JobItem.cs
+++ MyVideoResume.Abstractions/Job/JobItem.cs
@@ -7,10 +7,29 @@ using System.Threading.Tasks;
 
 namespace MyVideoResume.Abstractions.Job;
 
+
+public enum JobStatus
+{
+    Draft,
+    Open,
+    Closed,
+    Hired
+}
 public class JobItem : CommonBase
 {
     public string UserId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
 
+    public List<Industry> Industry { get; set; }
+    public List<ExperienceLevel> Seniority { get; set; } 
+    public List<JobType> EmploymentType { get; set; } = new List<JobType>();
+    public PaySchedule PaySchedule { get; set; } = PaySchedule.Yearly;
+    public float MinimumSalary { get; set; }
+    public float MaximumSalary { get; set; }
+
+    public DateTime GoLiveDate { get; set; }
+    public DateTime ExpirationDate { get; set; }
+
+    public JobStatus Status { get; set; }
 }
--- MyVideoResume.Data/Migrations/MyVideoResume/20241201190438_Job Posting.Designer.cs
+++ MyVideoResume.Data/Migrations/MyVideoResume/20241201190438_Job Posting.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyVideoResume.Data;
 
 #nullable disable
 
-namespace MyVideoResume.Server.Data.Migrations.MyVideoResume
+namespace MyVideoResume.Data.Migrations.MyVideoResume
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20241201190438_Job Posting")]
+    partial class JobPosting
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
