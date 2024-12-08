a07b72d719981aebfbe9a9f81ac4ab4ce4df8ca6
Sean Hogg <seanhogg@gmail.com>
2024-12-07 21:45:46 -0500

    MYV-14 Timeline

--- MyVideoResume.Data/Models/Resume/MetaResumeEntity.cs
+++ MyVideoResume.Data/Models/Resume/MetaResumeEntity.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using MyVideoResume.Abstractions.Resume.Formats.JSONResumeFormat;
 using Riok.Mapperly.Abstractions;
+using Microsoft.EntityFrameworkCore;
 
 namespace MyVideoResume.Data.Models.Resume;
 
@@ -19,7 +20,10 @@ public class MetaResumeEntity : JSONResume
     public List<MetaDataEntity> MetaData { get; set; }
 
     [ForeignKey("Resume")]
+    [DeleteBehavior(DeleteBehavior.Restrict)] 
     public ResumeInformationEntity ResumeInformation { get; set; }
+    
+    [DeleteBehavior(DeleteBehavior.Restrict)]
     public ResumeTemplateEntity? ResumeTemplate { get; set; }
     public DateTime? CreationDate { get; set; }
     public DateTime? UpdateTime { get; set; }
