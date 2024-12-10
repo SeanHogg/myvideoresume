ff81b521c84e49e0f3ee8c12a1f190c40d32fb8f
Sean Hogg <seanhogg@gmail.com>
2024-12-09 21:35:30 -0500

    MYV-37 Added Volunteering to timeline

--- Client/Pages/App/People/Resumes/Builder/BuilderPage.razor.cs
+++ Client/Pages/App/People/Resumes/Builder/BuilderPage.razor.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 using Microsoft.JSInterop;
+using MyVideoResume.Client.Services;
+using MyVideoResume.Data.Models.Resume;
 using Radzen;
 using Radzen.Blazor;
 
@@ -14,4 +16,41 @@ namespace MyVideoResume.Client.Pages.App.People.Resumes.Builder;
 
 public partial class BuilderPage
 {
+    [Parameter]
+    public String ResumeId { get; set; }
+
+
+    [Inject] ILogger<BuilderPage> Logger { get; set; }
+
+    public MetaResumeEntity Resume { get; set; } = new MetaResumeEntity();
+
+    public Type ComponentType { get; set; }
+    public Dictionary<string, object> ComponentParameters { get; set; }
+
+    [Inject] protected ResumeWebService Service { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            Resume = await Service.GetResume(ResumeId);
+
+            if (Resume != null)
+            {
+                if (Resume.ResumeTemplate != null)
+                {
+                    ComponentType = ResolveComponent(Resume.ResumeTemplate.TransformerComponentName, Resume.ResumeTemplate.Namespace);
+                    ComponentParameters = new Dictionary<string, object>() { { "resume", Resume } };
+                }
+                StateHasChanged();
+            }
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex.Message, ex);
+        }
+
+        await base.OnInitializedAsync();
+    }
+
 }
