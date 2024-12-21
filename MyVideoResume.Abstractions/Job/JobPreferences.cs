ffd2f223c4f270abc8e2aef5465933551b4b2df1
Sean Hogg <seanhogg@gmail.com>
2024-12-21 10:18:07 -0500

    MYV-44 Telemetry

--- MyVideoResume.Abstractions/Job/JobPreferences.cs
+++ MyVideoResume.Abstractions/Job/JobPreferences.cs
@@ -1,58 +1,96 @@
 ﻿using MyVideoResume.Abstractions.Core;
+using System.ComponentModel;
 
 namespace MyVideoResume.Abstractions.Job;
 
 public enum Industry
 {
-    AccountingFinance,
-    Administration,
-    ArtDesign,
-    BusinessSales,
-    Construction,
-    Education,
-    Engineering,
-    FoodProduction,
-    HealthCare,
-    HumanResources,
-    IT,
-    Legal,
-    Management,
-    Manufacturing,
-    MarketingPR,
-    RetailCustomerService,
-    Science,
-    SoftwareEngineering,
-    TransportationLogistics,
-    Hospitality,
-    Government
+    [Description("Accounting Finance")]
+    AccountingFinance = 0,
+    [Description("Administration")]
+    Administration = 1,
+    [Description("Art Design")]
+    ArtDesign = 2,
+    [Description("Business Sales")]
+    BusinessSales = 3,
+    [Description("Construction")]
+    Construction = 4,
+    [Description("Education")]
+    Education = 5,
+    [Description("Engineering")]
+    Engineering = 6,
+    [Description("Food Production")]
+    FoodProduction = 7,
+    [Description("Healthcare")]
+    Healthcare = 8,
+    [Description("Human Resources")]
+    HumanResources = 9,
+    [Description("IT")]
+    IT = 10,
+    [Description("Legal")]
+    Legal = 11,
+    [Description("Management")]
+    Management = 12,
+    [Description("Manufacturing")]
+    Manufacturing = 13,
+    [Description("Marketing / Public Relations (PR)")]
+    MarketingPR = 14,
+    [Description("Retail Customer Service")]
+    RetailCustomerService = 15,
+    [Description("Science")]
+    Science = 16,
+    [Description("Software Engineering")]
+    SoftwareEngineering = 17,
+    [Description("Transportation Logistics")]
+    TransportationLogistics = 18,
+    [Description("Hospitality")]
+    Hospitality = 19,
+    [Description("Government")]
+    Government = 20
 }
 
 public enum ExperienceLevel
 {
-    Entry,
-    Junior,
-    Mid,
-    Senior,
-    LeadExecutive,
-    None
+    [Description("Entry")]
+    Entry = 0,
+    [Description("Junior")]
+    Junior = 1,
+    [Description("Mid-Career")]
+    Mid = 2,
+    [Description("Senior")]
+    Senior = 3,
+    [Description("Executive")]
+    LeadExecutive = 4,
+    [Description("None")]
+    None = 5
 }
 
 public enum JobType
 {
-    FullTime,
-    PartTime,
-    Freelance,
-    Contractor,
-    ContractToHire,
-    Temporary,
-    Internship
+    [Description("Full-Time")]
+    FullTime = 0,
+    [Description("Part-Time")]
+    PartTime = 1,
+    [Description("Freelance")]
+    Freelance = 2,
+    [Description("Contractor")]
+    Contractor = 3,
+    [Description("Contract-To-Hire")]
+    ContractToHire = 4,
+    [Description("Temporary")]
+    Temporary = 5,
+    [Description("Internship")]
+    Internship = 6
 }
 
 public enum WorkSetting
 {
-    Onsite,
-    Remote,
-    Hybrid
+    [Description("Onsite")]
+    Onsite = 0,
+    [Description("Remote")]
+    Remote = 1,
+    [Description("Hybrid")]
+    Hybrid = 2
 }
 
 
